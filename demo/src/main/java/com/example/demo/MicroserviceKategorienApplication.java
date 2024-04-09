2e77de778f65af5360326076c3d41b48eb09a806
RuwenC <ruwen.christ@gmx.de>
2024-04-09 08:21:36 +0200

    microservice kategorien

---
+++ demo/src/main/java/com/example/demo/MicroserviceKategorienApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceKategorienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceKategorienApplication.class, args);
+	}
+
+}
