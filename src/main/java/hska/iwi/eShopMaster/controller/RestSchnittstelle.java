59b82aeca089ebea491f0eb393b13d2736835eeb
XhenZui <64024277+XhenZui@users.noreply.github.com>
2024-04-09 17:45:21 +0200

    add basic rest call to microservice

--- src/main/java/hska/iwi/eShopMaster/controller/RestSchnittstelle.java
+++ src/main/java/hska/iwi/eShopMaster/controller/RestSchnittstelle.java
@@ -1,5 +1,13 @@
 package hska.iwi.eShopMaster.controller;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -9,40 +17,24 @@ import java.net.URL;
 public class RestSchnittstelle {
 
     public static String makeRestcall() {
-
-        try {
-
-            URL url = new URL("http://localhost:8080/helloworld");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
-
-            if (conn.getResponseCode() >= 300) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + conn.getResponseCode());
+        try{
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet("http://microservice-kategorien:8080/helloworld");
+            CloseableHttpResponse response = httpClient.execute(httpGet);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            String line;
+            String alles = "";
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                alles += line;
             }
+            return alles;
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (conn.getInputStream())));
-            conn.disconnect();
-            //String output;
-            System.out.println("Output from Server .... \n");
-            System.out.println(br.toString());
-            return br.toString();
-           /*
-            while ((output = br.readLine()) != null) {
-               JsonObject json = (JsonObject) new JsonParser().parse(output);
-                String out = (String) json.get("userid").toString();
-                if (!out.equals("null")) {
-                    return "SUCCESS";
-                }
-            }#
-
-            */
-        } catch (Exception e){
+        } catch (IOException e) {
             e.printStackTrace();
-            return "";
         }
+
+        return "";
     }
 }
 
