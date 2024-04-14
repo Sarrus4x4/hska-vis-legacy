aac36e18a850bd4b194dddef220b1ebf46392ba8
RuwenC <ruwen.christ@gmx.de>
2024-04-14 20:21:10 +0200

    migrate list all products to microservices

--- src/main/java/hska/iwi/eShopMaster/controller/ListAllProductsAction.java
+++ src/main/java/hska/iwi/eShopMaster/controller/ListAllProductsAction.java
@@ -1,10 +1,13 @@
 package hska.iwi.eShopMaster.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import hska.iwi.eShopMaster.model.businessLogic.manager.ProductManager;
 import hska.iwi.eShopMaster.model.businessLogic.manager.impl.ProductManagerImpl;
+import hska.iwi.eShopMaster.model.database.dataobjects.Category;
 import hska.iwi.eShopMaster.model.database.dataobjects.Product;
 import hska.iwi.eShopMaster.model.database.dataobjects.User;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -29,14 +32,44 @@ public class ListAllProductsAction extends ActionSupport {
 		
 		if(user != null){
 			System.out.println("list all products!");
-			ProductManager productManager = new ProductManagerImpl();
-			this.products = productManager.getProducts();
+			//ProductManager productManager = new ProductManagerImpl();
+			//this.products = productManager.getProducts();
+			getFromMicroservice();
 			result = "success";
 		}
 		
 		return result;
 	}
-	
+
+	public void getFromMicroservice(){
+		JsonNode allProducts = RestHelper.getCall("Product-getall","product");
+
+		List<Product> products = new ArrayList<Product>();
+
+		for (JsonNode product : allProducts) {
+			int id = product.get("id").asInt();
+			String name = product.get("name").asText();
+			int productCategoryId = product.get("categoryId").asInt();
+
+			JsonNode catResult = RestHelper.getCall("Category/" + productCategoryId, "category");
+
+			String categoryName = catResult.get("name").asText();
+			int categoryId =  catResult.get("id").asInt();
+
+			Category category = new Category(categoryName,categoryId);
+
+			double price = product.get("price").asDouble();
+			String details =  product.get("details").asText();
+
+
+			Product newOne = new Product(id,name,price,category,details);
+			products.add(newOne);
+		}
+		this.products = products;
+	}
+
+
+
 	public User getUser() {
 		return user;
 	}
