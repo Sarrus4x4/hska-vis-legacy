40cddecca7a266d046c8563d3f41edb419499486
RuwenC <ruwen.christ@gmx.de>
2024-05-20 18:06:09 +0200

    make visible which pod answers

---
+++ Categories/src/main/java/hska/iwi/categories/ResponseHeaderFilter.java
@@ -0,0 +1,37 @@
+package hska.iwi.categories;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class ResponseHeaderFilter implements Filter {
+
+    private String pod;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // ...
+        pod = System.getenv("HOSTNAME");
+        if(pod == null) {
+            pod = "non-nada-niente";
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        httpServletResponse.setHeader(
+                "Kubernetes-Pod", pod);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        // ...
+    }
+}
